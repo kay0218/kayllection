cf344bf3d6c788a09ddb0b52f5c151da8d3a21a0
Kay <92441528+kay0218@users.noreply.github.com>
2025-04-26 20:40:30 +0100

    Changing image link

--- src/components/kay-logo.js
+++ src/components/kay-logo.js
@@ -5,7 +5,7 @@ import '../css/kay-logo.modules.scss';
 function KayLogo() {
     return (
         <div>
-            <img src="../../images/kay-logo.png" className="kay-logo" alt="site logo" />
+            <img src="/public/images/kay-logo.png" className="kay-logo" alt="site logo" />
             <a target="_blank" rel="noreferrer" href="https://open.spotify.com/track/6xLm5hdcUTmZPWUzPnQ9US?si=5ce1f4e897244863">
                 <p className="intro">Intro</p>
             </a>
